05fc9f29609b77c4ca677839bc757a5a428b9297
Cameron Maiden <cmaiden42@g.ucla.edu>
2024-06-02 19:14:41 -0700

    *Cleaned up solutions to functions in all the files so it can be turned in

--- s24Homework4/evenvector.cpp
+++ s24Homework4/evenvector.cpp
@@ -5,12 +5,6 @@
 //  Created by Cameron Maiden on 5/14/24.
 //
 
-#include <vector>
-#include <algorithm>
-#include <iostream>
-#include <cassert>
-using namespace std;
-
 // Remove the even integers from v.
 // It is acceptable if the order of the remaining even integers is not
 // the same as in the original vector.
@@ -44,23 +38,4 @@ void removeEven(vector<int>& v)
         else
             p++;
     }
-}
-
-void test()
-{
-    int a[9] = { 4, 1, 7, 6, 5, 8, 0, 3, 2 };
-    vector<int> x(a, a+9);  // construct x from the array
-    assert(x.size() == 9 && x.front() == 4 && x.back() == 2);
-    removeEven(x);
-    assert(x.size() == 4);
-    sort(x.begin(), x.end());
-    int expect[4] = { 1, 3, 5, 7 };
-    for (int k = 0; k < 4; k++)
-        assert(x[k] == expect[k]);
-}
-
-int main()
-{
-    test();
-    cout << "Passed" << endl;
 }
